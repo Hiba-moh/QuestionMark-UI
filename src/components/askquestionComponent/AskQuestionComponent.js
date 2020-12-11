4ac0577c424d016c8522c04481ddf14f8c73f7ab
Davinder <davibaweja@gmail.com>
2020-12-11 23:50:08 +0000

    created the ask-question component, added the heading and form and button

--- src/components/askquestionComponent/AskQuestionComponent.js
+++ src/components/askquestionComponent/AskQuestionComponent.js
@@ -1,10 +1,25 @@
 import React from "react"
-// import './AskQuestionQuestion.css'
+import './AskQuestionComponent.css'
 
 function AskQuestionQuestion()
 {
     return(<div>
-        Ask Question
+        <div className="first-section">
+            <h1 className="heading">Ask Question</h1>
+            <button className="logout-btn">Logout</button>
+        </div>
+        <div className="second-section">
+            <form className="title-module">
+                <input className="title" name="title" type="text" placeholder="Title of the question" required />
+                <input className="module"name="title" type="text" placeholder="Module" required />
+            </form>
+
+            <form className="text-post">
+                <textarea className="ques-text"name="title" type="text" placeholder="Type your question here" required/>
+                <input className="post-btn" type="submit" value="Post"/>
+            </form>
+
+        </div>
     </div>)
 }
 
