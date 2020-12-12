e33139687ebae25832f1e2a36bada5393dacb9cb
Your Name <hiba.moh@yahoo.com>
2020-12-12 15:29:11 +0000

    AllQuestions-AnsweredQuestion-UnansweredQuestion

---
+++ src/pages/unanswered/UnAnswered.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import UnAnsweredComponent
+  from '../../components/unansweredComponent/UnAnsweredComponent';
+
+const UnAnswered = () => {
+  return (
+    <div>
+      <UnAnsweredComponent />
+    </div>
+  );
+};
+
+export default UnAnswered;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import { Route, BrowserRouter } from 'react-router-dom';
+import {Route, BrowserRouter} from 'react-router-dom';
 import SignUp from '../pages/signup/Signup';
-import Login from '../pages/login/Login'
-
-
-
+import Login from '../pages/login/Login';
+import AllQuestions from '../pages/allquestions/AllQuestions';
+import Answered from '../pages/answered/Answered';
+import UnAnswered from '../pages/unanswered/UnAnswered';
 
 export default () => {
-    return (
-        <div>
-            <BrowserRouter>
-                <Route path="/signup" component={SignUp} />
-                <Route path="/login" component={Login} />
-            </BrowserRouter>
-        </div>
-    )
-}
+  return (
+    <div>
+      <BrowserRouter>
+        <Route path="/signup" component={SignUp} />
+        <Route path="/login" component={Login} />
+        <Route path="/allquestions" component={AllQuestions} />
+        <Route path="/answered" component={Answered} />
+        <Route path="/unAnswered" component={UnAnswered} />
+
+      </BrowserRouter>
+    </div>
+  );
+};
