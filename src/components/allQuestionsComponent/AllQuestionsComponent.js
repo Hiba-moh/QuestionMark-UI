7ed4950b2b21fd93c285d6a9a18134a4829cc172
Your Name <hiba.moh@yahoo.com>
2020-12-14 18:09:12 +0000

    Merge branch 'master' into feature/AllQuestions-FirstPage

--- src/components/allQuestionsComponent/AllQuestionsComponent.js
+++ src/components/allQuestionsComponent/AllQuestionsComponent.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import '../allQuestionsComponent/AllQuestionsComponent.css';
-import {Link} from 'react-router-dom';
+import {Link, useHistory} from 'react-router-dom';
 
-const allQuestionsComponent = () => {
+const AllQuestionsComponent = () => {
+  const [list, setList] = useState ([]);
+  const [filter, setFilter] = useState ([]);
+  const history = useHistory ();
+  useEffect (() => {
+    fetch (`https://api-test111.herokuapp.com/allquestions`)
+      .then (res => {
+        console.log (res);
+        if (!res.ok) {
+          throw Error (res.status + ' _ ' + res.url);
+        }
+        return res.json ();
+      })
+      .then (data => {
+        console.log (data.allquestions);
+        setList (data.allquestions);
+        setFilter (data.filter);
+      })
+      .catch (error => {
+        console.error (error);
+      });
+  }, []);
+
   return (
     <div>
       <div class="search-container">
@@ -19,7 +41,14 @@ const allQuestionsComponent = () => {
         </div>
         <div class="form2">
           <form>
-            <button class="ask-btn">Ask Question</button>
+            <button
+              class="ask-btn"
+              onClick={() => {
+                history.push ('/askquestion');
+              }}
+            >
+              Ask Question
+            </button>
           </form>
         </div>
       </div>
@@ -32,11 +61,18 @@ const allQuestionsComponent = () => {
       </ul>
 
       <div class="allquestions-container">
-        <div class="allquestions" />
+        <div class="allquestions1">
+          {list.map (question => (
+            <div class="question1">
+              <a href="/Unanswerd">{question.question}</a>
+            </div>
+          ))}
 
+        </div>
       </div>
+
     </div>
   );
 };
 
-export default allQuestionsComponent;
+export default AllQuestionsComponent;
