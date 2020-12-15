9886f83e6c7c4c36849283600fe678a30ed14c9b
Davinder <davibaweja@gmail.com>
2020-12-15 22:02:34 +0000

    Divided the content of the page into two seperate components , left section and a right section

---
+++ src/components/askquestionComponent/RightSection.js
@@ -0,0 +1,28 @@
+import React,{useState} from 'react';
+import './AskQuestionComponent.css'
+
+function RightSection()
+{
+    return(
+        <div className="right-section">
+            <div className="first-section">
+                <h1 className="heading">Ask Question</h1>
+                <button className="logout-btn">Logout</button>
+            </div>
+            <div className="second-section">
+                <form className="title-module">
+                    <input className="title" name="title" type="text" placeholder="Title of the question" required />
+                    <input className="module"name="title" type="text" placeholder="Module" required />
+                </form>
+
+                <form className="text-post">
+                    <textarea className="ques-text"name="title" type="text" placeholder="Type your question here" required/>
+                    <input className="post-btn" type="submit" value="Post"/>
+                </form>
+            </div>
+        </div>
+    )
+
+}
+
+export default RightSection;
