f75c4e00c7cead4a11f213ab6b86216d17bd5eb4
Davinder <davibaweja@gmail.com>
2020-12-15 22:13:35 +0000

    Moved all the text fields inside a same form tag

--- src/components/askquestionComponent/RightSection.js
+++ src/components/askquestionComponent/RightSection.js
@@ -9,17 +9,17 @@ function RightSection()
                 <h1 className="heading">Ask Question</h1>
                 <button className="logout-btn">Logout</button>
             </div>
-            <div className="second-section">
-                <form className="title-module">
+            <form className="second-section">
+                <div className="title-module">
                     <input className="title" name="title" type="text" placeholder="Title of the question" required />
                     <input className="module"name="title" type="text" placeholder="Module" required />
-                </form>
+                </div>
 
-                <form className="text-post">
+                <div className="text-post">
                     <textarea className="ques-text"name="title" type="text" placeholder="Type your question here" required/>
                     <input className="post-btn" type="submit" value="Post"/>
-                </form>
-            </div>
+                </div>
+            </form>
         </div>
     )
 
