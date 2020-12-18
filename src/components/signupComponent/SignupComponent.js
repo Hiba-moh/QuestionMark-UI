27065cafeb3a534d32613e932b3ca9a60113a939
sadatakhtar <sadat0akhtar@gmail.com>
2020-12-18 20:53:12 +0000

    fixed bug in signup page

--- src/components/signupComponent/SignupComponent.js
+++ src/components/signupComponent/SignupComponent.js
@@ -1,21 +1,37 @@
-import React, { useState } from 'react'
-import './LoginComponent.css';
-import { useHistory, Link } from 'react-router-dom';
+import React, {useState} from 'react'
+import Axios from 'axios';
+import './SignupComponent.css';
+import { useHistory } from 'react-router-dom';
 
 
+function SignupComponent() {
 
-function LoginComponent(props) {
-
-    const [logUsername, setLogUsername] = useState("");
-    const [logPassword, setLogPassword] = useState("");
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirm, setConfirm] = useState("");
+    const [email, setEmail] = useState("");
+    const [role, setRole] = useState("");
 
     const history = useHistory();
 
+    const register = () => {
+        Axios.post('https://question-mark-api.herokuapp.com/register', {
+            username: name, 
+            password: password,
+            email: email
+        }).then((response) => {
+            console.log(response);
+        })
+
+    };
+
     const details = {
-        username: logUsername, 
-         password: logPassword
-    
-     };
+       username: name, 
+        password: password,
+        confirm: confirm,
+        email: email
+        // role
+    };
 
     const options = {
         method: "POST",
@@ -25,61 +41,69 @@ function LoginComponent(props) {
         body: JSON.stringify(details)
     }
 
-   
-     const handleSubmit =  (e)  => {
+    const handleName = (e) => {
+        setName(e.target.value);
+    }
+    const handlePassword = (e) => {
+        console.log(e.target.value);
+        setPassword(e.target.value);
+    }
+    const handleConfirm = (e) => {
+        setConfirm(e.target.value);
+    }
+    const handleEmail = (e) => {
+        setEmail(e.target.value);
+    }
+    const handleRole = (e) => {
+        setRole(e.target.value);
+    }
+
+   // https://lowly-foam-badger.glitch.me/creatures
+    const handleSubmit = (e) => {
         e.preventDefault();
-        fetch('https://question-mark-api.herokuapp.com/login', options)
+        fetch('http://127.0.0.1:3003/register', options)
         .then(response => {
             return response.json();
         })
         .then(data => {
-            console.log(data);
-
-
-            if(data.success === false){
-                //localStorage.setItem("token", JSON.stringify(data)); //stores token in local storage
-                history.push('/signup');
-                
-            }else{
-                history.push('/allquestions');
-               
-            }  
+           data ? console.log({success: true}) : console.log({success: false});
+           if(data.success === true){
+            localStorage.setItem("token", JSON.stringify(data)); 
+            history.push('/login');
+           }else{
+            console.log('Registration failed');
+           }
+           
         })
         .catch(e => {
             console.error(e);
-        })   
-    }
-   
-    const handleLogUsername = (e) => {
-        setLogUsername(e.target.value);
+        })
+        
     }
 
-    const handleLogPassword = (e) => {
-        setLogPassword(e.target.value);
-    }
-    
     return (
-        <div className="login_container">
-            <div className="login_title">
-                <h2>Log In</h2>
-            </div>
-            <div className="login_form">
-                <form onSubmit={handleSubmit}>
-                    <input name="username" type="text" placeholder="Username" onChange={handleLogUsername} required /> 
-                    <input name="password" type="password" placeholder="Password" onChange={handleLogPassword} required /> 
-                    <div className="login_form_btn">
-                        <button type="submit">Login</button>
-                    </div>
-                    <div className="login_btn_links">
-                        <p><Link to="/signup">Sign up | Forgot password?</Link></p>
-                    </div>
-                </form>
-            </div>
+        <div className="signupComponent_container">
+             <div className="signup_title">
+               <h2>Sign up</h2>
+           </div>
+           <div className="signup_form">
+               <form onSubmit={handleSubmit} >
+                   <input  name="username" type="text" placeholder="Username"  onChange={handleName} required/>
+                   <input name="email" type="email" placeholder="email" onChange={handleEmail} required/>
+                   <input name="password" type="password" placeholder="Password" onChange={handlePassword} required/>
+                   <input name="confirm" type="password" placeholder="Confirm password" onChange={handleConfirm} required/>
+                   {/* <input name="role" type="text" placeholder="Student/Teacher" onChange={handleRole} required/> */}
+                   <div className="signup_form_btn">
+                       <button  type="submit">Register</button>
+                   </div>
+               </form>
+           </div>
+           
             
         </div>
     )
 }
 
-export default LoginComponent
+export default SignupComponent;
 
 
