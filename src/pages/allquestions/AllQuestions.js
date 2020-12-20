b8ccee087643699b123df782ee3850189f840061
Your Name <hiba.moh@yahoo.com>
2020-12-20 16:55:10 +0000

    added footer

--- src/pages/allquestions/AllQuestions.js
+++ src/pages/allquestions/AllQuestions.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import AllQuestionsComponent
   from '../../components/allQuestionsComponent/AllQuestionsComponent';
+import Footer from '../../components/footerComponent/Footer';
 
 function AllQuestions () {
   return (
     <div>
       <AllQuestionsComponent />
+      <Footer />
     </div>
   );
 }
