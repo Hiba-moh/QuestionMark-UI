f634df3f291582ac0ce0036e7d43ff6edee92f69
Your Name <hiba.moh@yahoo.com>
2020-12-20 16:59:16 +0000

    Added the header

--- src/pages/allquestions/AllQuestions.js
+++ src/pages/allquestions/AllQuestions.js
@@ -2,10 +2,12 @@ import React from 'react';
 import AllQuestionsComponent
   from '../../components/allQuestionsComponent/AllQuestionsComponent';
 import Footer from '../../components/footerComponent/Footer';
+import Header from '../../components/headerComponent/Header';
 
 function AllQuestions () {
   return (
     <div>
+      <Header />
       <AllQuestionsComponent />
       <Footer />
     </div>
