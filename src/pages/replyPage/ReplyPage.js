be5721f8a58caa98d58fc577c91e2d086560fc2a
sadatakhtar <sadat0akhtar@gmail.com>
2020-12-27 12:57:19 +0000

    Added context for isAuth/setIsAuth, changed app.js => added all the routes from index.js, loginComponent adjusted to use useContext(isAuth/setIsAuth), created protectedRoutes component, adjusted replypage to use useContext (isAuth/setIsAuth), configured logout button to setIsAuth to false on click, Added slack message function to reply button in replypage

--- src/pages/replyPage/ReplyPage.js
+++ src/pages/replyPage/ReplyPage.js
@@ -4,9 +4,39 @@ import SidebarComponent from '../../components/sidebarComponent/SidebarComponent
 import TextareaComponent from '../../components/textareaComponent/TextareaComponent';
 import ButtonComponent from '../../components/buttonComponent/ButtonComponent';
 import Header from '../../components/headerComponent/Header'
+import {AuthContext} from '../../AuthContext';
 
 
 function ReplyPage() {
+
+    const [isAuth, setIsAuth] = useContext(AuthContext);
+
+    //CONTAINS BODY OF SLACK MESSAGE
+    const data = {
+        channel: "#questionmark_forum",
+        attachments: [{
+            color: "danger",
+            fields: [{
+                title: "Question No.5006 username: @Sadat Topic: TESTING123",
+                value: "Your question has a reply. Please sign in to the question forum to check your answer.",
+                short: false
+            }]
+        }]
+    }
+
+    async function handleSlackMessage(){
+        let res = await Axios.post('https://hooks.slack.com/services/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', JSON.stringify(data), {
+            withCredentials: false,
+            transformRequest: [(data, headers) => {
+                delete headers.post["Content-Type"]
+                return data;
+            }]
+        })
+        res.status === 200 ? (alert('Sending Slack notification...')):(alert('Error sending message'));
+        
+       
+    }
+
     return (
         <div className="reply_outer_container">
             <div className="reply_header">
@@ -22,7 +52,8 @@ function ReplyPage() {
                     <h2>Reply to the question</h2>
                 </div>
                 <div className="reply_btn">
-                    <ButtonComponent label="Logout"/>
+                    {/* <ButtonComponent label="Logout"/> */}
+                    <button onClick={()=> setIsAuth(false)}>Logout</button>
                 </div>
 
                 </div>
