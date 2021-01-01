f17aaaded13811dac5a2ea6db3fd0944b4f36a8a
sadatakhtar <sadat0akhtar@gmail.com>
2021-01-01 22:05:55 +0000

    fixed slack message component to send message after server response and also redirected to  /allquestions page after submitting answer

--- src/pages/replyPage/ReplyPage.js
+++ src/pages/replyPage/ReplyPage.js
@@ -1,123 +1,123 @@
-import React, {useEffect, useContext} from 'react';
+import React, {useEffect} from 'react';
 import './ReplyPage.css';
 import SidebarComponent
   from '../../components/sidebarComponent/SidebarComponent';
+import TextEditor from '../../components/replyComponent/TextEditor';
 import Header from '../../components/allQuestionsComponent/Header';
 import Footer from '../../components/footerComponent/Footer';
-import {useState} from 'react/cjs/react.development';
+import {useState, useContext} from 'react/cjs/react.development';
 import {mockComponent} from 'react-dom/test-utils';
 import moment from 'moment';
 import axios from 'axios';
 import '../../components/replyComponent/UserAnswered';
 import '../../components/replyComponent/UserAsked';
-//import {withRouter} from 'react-router-dom';
+import {withRouter, useHistory} from 'react-router-dom';
 import {AuthContext} from '../../AuthContext';
-
-
-
-import TextEditor from '../../components/replyComponent/TextEditor';
-import {useHistory, Link, withRouter} from 'react-router-dom';
 
 function ReplyPage({match}) {
   const id = match.params.id;
   const [answer, SetAnswer] = useState ('');
   const [questionReply, SetQuestionReply] = useState ('');
   //const [isAuth, setIsAuth] = useContext(AuthContext);
-  const {isAuth, greet, idNumber} = useContext(AuthContext); 
+  const {isAuth, greet, idNumber} = useContext (AuthContext);
   const [isAuthValue, setIsAuthValue] = isAuth;
-  const [greetValue, setGreetValue] = greet; 
+  const [greetValue, setGreetValue] = greet;
   const [idNumberValue, setIdNumberValue] = idNumber;
+  const history = useHistory ();
 
-
-  //DISPLAYS THE QUESTION ON REPLYPAGE
   const questionToReplyById = axios
     .get (`https://question-mark-api.herokuapp.com/selectedquestionpage/${id}`)
     .then (response => SetQuestionReply (response.data.question[0].question))
     .catch (error => console.log (error));
 
-    //POSTS THE ANSWER TO THE BACKEND
-  const onSubmitForm = async e => {
-    // e.preventDefault ();
-    try {
-      const data = {
-        question_id: id,
-        reply: answer,
-        user_id: 1,
-        date: moment ().format ('YYYY/MM/DD'),
-      };
-      // axios.post (
-      //   `https://question-mark-api.herokuapp.com/replypage`,
-      //   JSON.stringify (data),
-      //   {
-      //     withCredentials: false,
-      //     transformRequest: [
-      //       (data, headers) => {
-      //         delete headers.post['Content-Type'];
-      //         return data;
-      //       },
-      //     ],
-      //   }
-      // );
-
-      console.log (data);
-      const response = await axios.post(
-        'https://question-mark-api.herokuapp.com/replypage',
-        {
-          method: 'POST',
-          body: JSON.stringify (data),
-          mode: 'cors',
-          // cache: 'no-cache',
-          headers: {'Content-Type': 'application/json'},
-        }
-      ).then(response => {
-          console.log(response);
-      })
+    const data1 = {
+          channel: "#questionmark_forum",
+          attachments: [{
+              color: "danger",
+              fields: [{
+                  title: "Question No.5007 username: @user Topic: TESTING123",
+                  value: "Your question has a reply. Please sign in to the question forum to check your answer.",
+                  short: false
+              }]
+          }]
+      }
       
-      //slack message
-      console.log (response);
-    } catch (err) {
-      console.error (err);
-    }
-  };
+      async function handleSlackMessage(){
+          let res = await axios.post(process.env.REACT_APP_API_KEY, JSON.stringify(data1), {
+              withCredentials: false,
+              transformRequest: [(data, headers) => {
+                  delete headers.post["Content-Type"]
+                  return data;
+              }]
+          })
+          res.status === 200 ? (alert('Sent Slack notification...')):(alert('Error sending message'));
+          
+       
+      }
 
-  // const questionToReply = async id => {
-  //   const res = await fetch`https://question-mark-api.herokuapp.com//selectedquestionpage/${id}`;
-  //   const resObj = await res.json ();
-  //   console.log ('question to reply = ', resObj.question[0].question);
-  //   SetQuestionReply (resObj.question.question[0].question);
+      const onSubmitForm = (e) => {
+        e.preventDefault();
+
+        const data = {
+                question_id: id,
+                reply: answer,
+                user_id: 1,
+                date: moment ().format ('YYYY/MM/DD'),
+              };
+
+              fetch('https://question-mark-api.herokuapp.com/replypage', {
+                method: 'POST',
+                        body: JSON.stringify (data),
+                        mode: 'cors',
+                        // cache: 'no-cache',
+                        headers: {'Content-Type': 'application/json'},
+
+              })
+              .then(response => {
+                return response.json();
+              })
+              .then(data => {
+                console.log(data.answer);
+                if(data.answer){
+                  handleSlackMessage();
+                  history.push('/allquestions');
+                }else{
+                  alert('Oops, something went wrong!');
+                  history.push('/allquestions');
+                }
+              })
+              .catch(err => {
+                console.error(err);
+              })
+
+      }
+
+  // const onSubmitForm = async e => {
+  //   // e.preventDefault ();
+  //   try {
+  //     const data = {
+  //       question_id: id,
+  //       reply: answer,
+  //       user_id: 1,
+  //       date: moment ().format ('YYYY/MM/DD'),
+  //     };
+
+  //     const response = await fetch (
+  //       'https://question-mark-api.herokuapp.com/replypage',
+  //       {
+  //         method: 'POST',
+  //         body: JSON.stringify (data),
+  //         mode: 'cors',
+  //         // cache: 'no-cache',
+  //         headers: {'Content-Type': 'application/json'},
+  //       }
+  //     );
+  //     response.status == 200 ? (handleSlackMessage()) : (alert('error'));
+  //    // console.log ('ReplyPage-Post-Response: ', response);
+  //   } catch (err) {
+  //     console.error (err);
+  //   }
   // };
-  // useEffect (() => {
-  //   questionToReply (id);
-  // }, []);
-  const data = {
-    channel: "#questionmark_forum",
-    attachments: [{
-        color: "danger",
-        fields: [{
-            title: "Question No.5007 username: @user Topic: TESTING123",
-            value: "Your question has a reply. Please sign in to the question forum to check your answer.",
-            short: false
-        }]
-    }]
-}
-
-async function handleSlackMessage(){
-    let res = await Axios.post(process.env.REACT_APP_API_KEY, JSON.stringify(data), {
-        withCredentials: false,
-        transformRequest: [(data, headers) => {
-            delete headers.post["Content-Type"]
-            return data;
-        }]
-    })
-    res.status === 200 ? (alert('Sent Slack notification...')):(alert('Error sending message'));
-    
-   
-}
-
-  const handleSubmit = () => {
-    console.log("clicked submit button!");
-  }
-
 
   return (
     <div className="ReplyPageContainer">
@@ -128,25 +128,25 @@ async function handleSlackMessage(){
           <h2>
             Reply to the question:
             {' '}
-            <h4><small class="text-muted">{questionReply}</small></h4>
+            <div>
+              {' '}
+              <h4><small className="text-muted">{questionReply}</small></h4>
+            </div>
           </h2>
-          <form id="ReplyForm" >
-            <label for="QuestionReply">Add your reply here ...</label>
+          <form id="ReplyForm" onSubmit={onSubmitForm}>
+            <label htmlFor="QuestionReply">Add your reply here ...</label>
 
             <TextEditor SetAnswer={SetAnswer} />
-            {/* 
 
-            <textarea
+            {/* <textarea
               id="QReply"
               name="Qreply"
               rows="10"
               cols="150"
               value={answer}
               onChange={e => SetAnswer (e.target.value)}
-
             /> */}
             <input id="ReplySubmitbtn" type="submit" value="Submit" />
-
           </form>
         </div>
       </div>
@@ -155,6 +155,153 @@ async function handleSlackMessage(){
   );
 }
 
+export default withRouter (ReplyPage);
 
-export default withRouter(ReplyPage);
+// import React, {useEffect, useContext} from 'react';
+// import './ReplyPage.css';
+// import SidebarComponent
+//   from '../../components/sidebarComponent/SidebarComponent';
+// import Header from '../../components/allQuestionsComponent/Header';
+// import Footer from '../../components/footerComponent/Footer';
+// import {useState} from 'react/cjs/react.development';
+// import {mockComponent} from 'react-dom/test-utils';
+// import moment from 'moment';
+// import axios from 'axios';
+// import '../../components/replyComponent/UserAnswered';
+// import '../../components/replyComponent/UserAsked';
+// //import {withRouter} from 'react-router-dom';
+// import {AuthContext} from '../../AuthContext';
+
+
+
+// import TextEditor from '../../components/replyComponent/TextEditor';
+// import {useHistory, Link, withRouter} from 'react-router-dom';
+
+// function ReplyPage({match}) {
+//   const id = match.params.id;
+//   const [answer, SetAnswer] = useState ('');
+//   const [questionReply, SetQuestionReply] = useState ('');
+//   //const [isAuth, setIsAuth] = useContext(AuthContext);
+//   const {isAuth, greet, idNumber} = useContext(AuthContext); 
+//   const [isAuthValue, setIsAuthValue] = isAuth;
+//   const [greetValue, setGreetValue] = greet; 
+//   const [idNumberValue, setIdNumberValue] = idNumber;
+
+
+//   //DISPLAYS THE QUESTION ON REPLYPAGE
+//   const questionToReplyById = axios
+//     .get (`https://question-mark-api.herokuapp.com/selectedquestionpage/${id}`)
+//     .then (response => SetQuestionReply (response.data.question[0].question))
+//     .catch (error => console.log (error));
+
+//   //POSTS THE ANSWER TO THE BACKEND
+//   const onSubmitForm = async e => {
+//      e.preventDefault ();
+//     try {
+//       const data = {
+//         question_id: id,
+//         reply: answer,
+//         user_id: 1,
+//         date: moment ().format ('YYYY/MM/DD'),
+//       };
+    
+
+//      // console.log (data);
+//       const response = await axios.post(
+//         'https://question-mark-api.herokuapp.com/replypage',
+//         {
+//           method: 'POST',
+//           body: JSON.stringify (data),
+//           mode: 'cors',
+//           // cache: 'no-cache',
+//           headers: {'Content-Type': 'application/json'},
+//         }
+//       )
+
+//       response.status == 200 ? (handleSlackMessage()): (alert('Error sending reply to DB'));
+
+
+      
+     
+    
+//     } catch (err) {
+//       console.error (err);
+//     }
+//   };
+
+//   // const questionToReply = async id => {
+//   //   const res = await fetch`https://question-mark-api.herokuapp.com//selectedquestionpage/${id}`;
+//   //   const resObj = await res.json ();
+//   //   console.log ('question to reply = ', resObj.question[0].question);
+//   //   SetQuestionReply (resObj.question.question[0].question);
+//   // };
+//   // useEffect (() => {
+//   //   questionToReply (id);
+//   // }, []);
+//   const data = {
+//     channel: "#questionmark_forum",
+//     attachments: [{
+//         color: "danger",
+//         fields: [{
+//             title: "Question No.5007 username: @user Topic: TESTING123",
+//             value: "Your question has a reply. Please sign in to the question forum to check your answer.",
+//             short: false
+//         }]
+//     }]
+// }
+
+// async function handleSlackMessage(){
+//     let res = await axios.post(process.env.REACT_APP_API_KEY, JSON.stringify(data), {
+//         withCredentials: false,
+//         transformRequest: [(data, headers) => {
+//             delete headers.post["Content-Type"]
+//             return data;
+//         }]
+//     })
+//     res.status === 200 ? (alert('Sent Slack notification...')):(alert('Error sending message'));
+    
+ 
+// }
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     // console.log("clicked submit button!");
+//    // handleSlackMessage();
+//   }
+
+
+//   return (
+//     <div className="ReplyPageContainer">
+//       <Header />
+//       <div className="reply-container">
+//         <SidebarComponent />
+//         <div className="replyBody">
+//           <h2>
+//             Reply to the question:
+//             {' '}
+//             <h4><small class="text-muted">{questionReply}</small></h4>
+//           </h2>
+//           <form id="ReplyForm" onSubmit={onSubmitForm} >
+//             <label for="QuestionReply">Add your reply here ...</label>
+
+//             <TextEditor SetAnswer={SetAnswer} />
+//             {/* 
+//  response.status == 200 ? (handleSlackMessage()): (alert('Error sending reply to DB'));    cols="150"
+//               value={answer}
+//               onChange={e => SetAnswer (e.target.value)}
+
+//             /> */}
+//             <input id="ReplySubmitbtn" type="submit" value="Submit" />
+
+//           </form>
+//           <p>{idNumber}</p>
+//         </div>
+//       </div>
+//       <Footer />
+//     </div>
+//   );
+// }
+
+
+// export default withRouter(ReplyPage);
 
