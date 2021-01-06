21f6605b0deff8ebd975b43c9b62e1dcb830eabd
Davinder <davibaweja@gmail.com>
2021-01-06 10:19:00 +0000

    changing the css to match the theme

--- src/pages/askquestion/AskQuestion.js
+++ src/pages/askquestion/AskQuestion.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useParams } from "react-router-dom";
 import AskQuestionComponent from '../../components/askquestionComponent/AskQuestionComponent';
+import Header from '../../components/allQuestionsComponent/Header';
 
 //send This id as a parameter to the askQuestion component and then use it in their.
 function AskQuestion() {
@@ -8,6 +9,7 @@ function AskQuestion() {
     console.log(id);
     return (
         <div>
+            <Header />
             <AskQuestionComponent  />
         </div>
     )
