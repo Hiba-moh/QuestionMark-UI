0ddd7beaac95493f097bc35dea127fc1ea3c2f9b
Davinder <davibaweja@gmail.com>
2021-01-07 21:37:18 +0000

    added the overlay to the tips button

--- src/components/askquestionComponent/Tips.js
+++ src/components/askquestionComponent/Tips.js
@@ -1,13 +1,39 @@
-import React from "react"
+import React,{useState} from "react"
 import "./Tips.css"
 
+// function Tips()
+// {
+//     return (
+//         <div className="tips-circle">
+//             <p className="tips-text">Tips For asking a good question</p>
+//         </div>
+//     )
+// }
 function Tips()
 {
+    const [expand,setExpand]=useState(false);
+    let overlayclass="";
+
+    function closeNav() {
+        setExpand(false);
+    }
+    function handleClick()
+    {
+        setExpand(true);
+    }
+
+    expand? (overlayclass="overlay"):(overlayclass="overlay-hidden")
     return (
         <div className="tips-circle">
-            <p className="tips-text">Tips For asking a good question</p>
+            <div id="myNav" class={overlayclass}>
+                <button className="closebtn" onClick={closeNav}>&times;</button>
+                <div className="overlay-content">
+                    <h1>Tips for a good ques hahah</h1>
+                    <p></p>
+                </div>
+            </div>
+            <button className="tips-text" onClick={handleClick}> Tips For asking a good question</button>
         </div>
     )
 }
-
 export default Tips;
