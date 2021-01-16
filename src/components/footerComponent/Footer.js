464fc0af4e7c49606facfeae2e75f051f81ba0aa
Hiba <57302997+Hiba-moh@users.noreply.github.com>
2021-01-16 20:55:43 +0000

    Update Footer.js

--- src/components/footerComponent/Footer.js
+++ src/components/footerComponent/Footer.js
@@ -6,7 +6,7 @@ function Footer () {
     <div className="footer_container">
       <footer className="footer">
         <h5>
-          Copyright question-mark | All reserved |Powered by CYF-team QuestionMark
+          © Copyright question-mark | All reserved |Powered by CYF-team QuestionMark
         </h5>
       </footer>
     </div>
